aa8fa56a35ad54261d630b2300afc3f991dbc35e
Ahmet Arşiv <101526596+ahmetarsiv@users.noreply.github.com>
2022-10-09 21:03:49 +0300

    Update IyzicoApi.php

--- src/Helpers/IyzicoApi.php
+++ src/Helpers/IyzicoApi.php
@@ -11,9 +11,9 @@ class IyzicoApi
 {
     public static function options() {
         $options = new \Iyzipay\Options();
-        $options->setApiKey("env('API_KEY')");
-        $options->setSecretKey("env('SECRET_KEY')");
-        $options->setBaseUrl("env('BASE_URL')");
+        $options->setApiKey("env('IYZICO_API_KEY')");
+        $options->setSecretKey("env('IYZICO_SECRET_KEY')");
+        $options->setBaseUrl("env('IYZICO_BASE_URL')");
         return $options;
     }
 }
