71fff9009753e46f821f42d09093ff776e6fa432
Ahmet Arşiv <101526596+ahmetarsiv@users.noreply.github.com>
2022-10-13 10:23:48 +0300

    Update IyzicoApi.php

--- src/Helpers/IyzicoApi.php
+++ src/Helpers/IyzicoApi.php
@@ -6,14 +6,14 @@ namespace Webkul\IyzicoPayment\Helpers;
 /**
  * Iyzico payment api and secret key
  */
-
 class IyzicoApi
 {
-    public static function options() {
+    public static function options()
+    {
         $options = new \Iyzipay\Options();
-        $options->setApiKey("env('IYZICO_API_KEY')");
-        $options->setSecretKey("env('IYZICO_SECRET_KEY')");
-        $options->setBaseUrl("env('IYZICO_BASE_URL')");
+        $options->setApiKey(env('IYZICO_API_KEY', 'null'));
+        $options->setSecretKey(env('IYZICO_SECRET_KEY', 'null'));
+        $options->setBaseUrl(env('IYZICO_BASE_URL', 'https://sandbox-api.iyzipay.com'));
         return $options;
     }
 }
