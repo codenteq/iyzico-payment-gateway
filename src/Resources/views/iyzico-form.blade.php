6b8c3133b78eaaa0fe5e876f24e6e6ac5bae9104
Ahmet Arşiv <101526596+ahmetarsiv@users.noreply.github.com>
2022-10-13 11:25:17 +0300

    Update iyzico-form.blade.php

--- src/Resources/views/iyzico-form.blade.php
+++ src/Resources/views/iyzico-form.blade.php
@@ -1,10 +1,12 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="content-language" content="{{ app()->getLocale() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>{{ __('iyzico::app.resources.title') }}</title>
 
     @if ($favicon = core()->getCurrentChannel()->favicon_url)
